c03c755f5cfb4aee3510a342c710524258da6e26
BasheerAhmed141 <basheerm2006@gmail.com>
2025-06-28 23:15:01 +0530

    string,java

---
+++ string.java
@@ -0,0 +1,17 @@
+ public class string{
+    public static void main(String[] args) {
+        
+
+        String s1 = "basheer";
+
+        s1 = s1+ " Ahmed";
+
+        System.out.println(s1);
+
+
+        String s2 = "BAS";
+        String s3 = "bas";
+
+        System.out.println(s2 == s3);
+    }
+}
