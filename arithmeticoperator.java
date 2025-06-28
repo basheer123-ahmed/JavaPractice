661cf8d1af0aba38eff74273d7e0cf76990f432b
BasheerAhmed141 <basheerm2006@gmail.com>
2025-06-28 23:54:17 +0530

    arithmeticoperator.java

---
+++ arithmeticoperator.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+public class arithmeticoperator{
+    public static void main(String[] args){
+
+
+        int num = 40;
+
+        //int result = ++num;     
+
+        //System.out.println(result);    // output:- 41 (pre)
+
+        int result1 = num++;
+
+        System.out.println(result1);    // output:- 40  (post)
+        
+    }
+
+}
