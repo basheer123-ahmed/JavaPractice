ffbd8751d7b1f65bb23f94923baf155c6fb95ee6
BasheerAhmed141 <basheerm2006@gmail.com>
2025-06-29 22:54:01 +0530

    stringbuffer.java

---
+++ stringbuffer.java
@@ -0,0 +1,18 @@
+public class stringbuffer{
+    public static void main(String [] args){
+
+        StringBuffer s1 = new StringBuffer(" Hello");
+
+        s1.append(" AHMED");
+        s1.delete(1, 1);
+        s1.length();
+        s1.replace(0, 0, "MOHAN BABU UNIVERSITY");
+        System.out.println(s1);
+
+
+
+
+
+        
+    }
+}
