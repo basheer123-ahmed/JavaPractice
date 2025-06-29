c7d7d9aa39e4d3ff59d6f70193acb06fb1165cdd
BasheerAhmed141 <basheerm2006@gmail.com>
2025-06-29 23:06:57 +0530

    reverseAString.java

---
+++ reverseAString.java
@@ -0,0 +1,14 @@
+public class reverseAString {
+    public static void main(String[] args) {
+        String s1 = "Basheer";
+        String reversed = "";
+
+        for(int i= s1.length()-1 ; i>=0; i--){
+            reversed +=s1.charAt(i);
+        }
+        System.out.println(reversed);
+        
+
+    }
+    
+}
